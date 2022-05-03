b881a020f280eef0b16bf0ac5b3939617ea62a90
gokulraj17-2000 <gokulrajkingdom@gmail.com>
2022-05-03 20:08:05 +0530

    added class

---
+++ src/test/java/com/test/GitDemo.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class GitDemo {
+	private void test1() {
+		System.out.println("1");
+	}
+
+	private void test2() {
+		System.out.println("2");
+	}
+
+	private void test3() {
+		System.out.println("2");
+	}
+
+}
